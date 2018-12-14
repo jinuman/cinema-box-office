6364db72963deff724853a7a0cdb3dffb012225b
jinuman <jin35kim@gmail.com>
2018-12-14 22:29:06 +0900

    feat: Design cell

---
+++ BoxOffice/Sources/Controllers/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  BoxOffice
+//
+//  Created by Jinwoo Kim on 2018. 12. 14..
+//  Copyright © 2018년 jinuman. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
